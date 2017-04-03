3a598d2669ef8da2d5c68aa9055ee2d9a5f9b8ed
WilliamBaumbach1212 <will@dailygrinder.com>
2017-04-03 16:59:09 -0400

    Added standings, and worked on conferences

--- src/Conference.java
+++ src/Conference.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Conference 
 {
 	private ArrayList<Team> _conferenceTeams = new ArrayList<Team>();
+	Standings standings = new Standings();
 	
 	public Conference(ArrayList<Team> conference)
 	{
--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,52 @@ public class Main {
 
 	private static boolean running = true;
 	
-	// Easter Conference Settup
-	public static Team bucks = new Team();
-	Team bulls = new Team();
-	Team cavaliers = new Team();
-	Team celtics = new Team();
-	Team hawks = new Team();
-	Team heat = new Team();
-	Team hornets = new Team();
-	Team knicks = new Team();
-	Team magic = new Team();
-	Team nets = new Team();
-	Team pacers = new Team();
-	Team pistons = new Team();
-	Team raptors = new Team();
-	Team seventysixers = new Team();
-	Team wizards = new Team();
-
-	ArrayList<Team> ec = new ArrayList<Team>(Arrays.asList(bucks, bulls, cavaliers, celtics, hawks, heat, hornets, knicks, 
+	// Easter Conference Setup
+	static Team bucks = new Team();
+	static Team bulls = new Team();
+	static Team cavaliers = new Team();
+	static Team celtics = new Team();
+	static Team hawks = new Team();
+	static Team heat = new Team();
+	static Team hornets = new Team();
+	static Team knicks = new Team();
+	static Team magic = new Team();
+	static Team nets = new Team();
+	static Team pacers = new Team();
+	static Team pistons = new Team();
+	static Team raptors = new Team();
+	static Team seventysixers = new Team();
+	static Team wizards = new Team();
+	static ArrayList<Team> ec = new ArrayList<Team>(Arrays.asList(bucks, bulls, cavaliers, celtics, hawks, heat, hornets, knicks, 
 			magic, nets, pacers, pistons, raptors, seventysixers, wizards));
+	
+	// Western Conference Setup
+	static Team clippers = new Team();
+	static Team grizzlies = new Team();
+	static Team jazz = new Team();
+	static Team kings = new Team();
+	static Team lakers = new Team();
+	static Team mavericks = new Team();
+	static Team nuggets = new Team();
+	static Team pelicans = new Team();
+	static Team rockets = new Team();
+	static Team spurs = new Team();
+	static Team suns = new Team();
+	static Team thunder = new Team();
+	static Team timberwolves = new Team();
+	static Team trailblazers = new Team();
+	static Team warriors = new Team();
+	static ArrayList<Team> wc = new ArrayList<Team>(Arrays.asList(clippers, grizzlies, jazz,
+			kings, lakers, mavericks, nuggets, pelicans, rockets, spurs, suns, thunder,
+			timberwolves, trailblazers, warriors));
 	
 	
 	public static void main(String[] args) 
 	{
+		Conference Eastern = new Conference(ec);
+		Conference Western = new Conference(wc);
 		
-		Conference Eastern = new Conference();
-		Conference Western = new Conference();
-		
+	
 		while(running)
 		{
 			
